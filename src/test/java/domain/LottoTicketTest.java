f4113a851f3d9e81c4124ea573125f991a529f0e
이한울 <dkfghg3@naver.com>
2022-03-03 15:35:35 +0900

    test: 로또 티켓 테스트 코드 추가 당첨된 로또가 하나도 없는 경우 구매한 로또가 없는 경우

--- src/test/java/domain/LottoTicketTest.java
+++ src/test/java/domain/LottoTicketTest.java
@@ -3,6 +3,7 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.EnumMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,23 +13,22 @@ import org.junit.jupiter.api.Test;
 public class LottoTicketTest {
 
     private WinningNumbers winningNumbers;
-    private LottoTicket myLottoTicket;
 
     @BeforeEach
     void setUpWinningNumbersAndLottoTicket() {
         Number bonusNumber = new Number(7);
         Lotto winningNumber = Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 6));
         winningNumbers = new WinningNumbers(winningNumber, bonusNumber);
-        myLottoTicket = new LottoTicket(Arrays.asList(
-                Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 7))
-        ));
     }
 
     @Test
-    @DisplayName("로또 티켓은 순위별 당첨 수를 계산한다")
+    @DisplayName("로또 티켓은 순위별 당첨 수를 계산한다.")
     void findWinningCountByRank() {
-        EnumMap<LottoRank, Integer> expectedWinningCountByRank = new EnumMap<>(Map.ofEntries(
+        final LottoTicket myLottoTicket = new LottoTicket(Arrays.asList(
+                Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 7))
+        ));
+        final EnumMap<LottoRank, Integer> expectedWinningCountByRank = new EnumMap<>(Map.ofEntries(
                 Map.entry(LottoRank.FIRST, 1),
                 Map.entry(LottoRank.SECOND, 1),
                 Map.entry(LottoRank.THIRD, 0),
@@ -38,6 +38,43 @@ public class LottoTicketTest {
 
         final EnumMap<LottoRank, Integer> actual = myLottoTicket.findWinningCountByRank(winningNumbers);
 
-        assertThat(expectedWinningCountByRank).isEqualTo(actual);
+        assertThat(actual).isEqualTo(expectedWinningCountByRank);
+    }
+
+    @Test
+    @DisplayName("당첨된 로또가 하나도 없는 경우, 순위별 당첨 수는 모두 0이다")
+    void findWinningCountOfFailLottoTicket() {
+        final LottoTicket myLottoTicket = new LottoTicket(Arrays.asList(
+                Lotto.from(Arrays.asList(10, 11, 12, 13, 14, 15)),
+                Lotto.from(Arrays.asList(1, 30, 31, 45, 39, 32))
+        ));
+        final EnumMap<LottoRank, Integer> expectedWinningCountByRank = new EnumMap<>(Map.ofEntries(
+                Map.entry(LottoRank.FIRST, 0),
+                Map.entry(LottoRank.SECOND, 0),
+                Map.entry(LottoRank.THIRD, 0),
+                Map.entry(LottoRank.FOURTH, 0),
+                Map.entry(LottoRank.FIFTH, 0)
+        ));
+
+        final EnumMap<LottoRank, Integer> actual = myLottoTicket.findWinningCountByRank(winningNumbers);
+
+        assertThat(actual).isEqualTo(expectedWinningCountByRank);
+    }
+
+    @Test
+    @DisplayName("구매한 로또가 없는 경우, 순위별 당첨 수는 모두 0이다")
+    void findWinningCountOfEmptyLottoTicket() {
+        final LottoTicket myLottoTicket = new LottoTicket(Collections.emptyList());
+        final EnumMap<LottoRank, Integer> expectedWinningCountByRank = new EnumMap<>(Map.ofEntries(
+                Map.entry(LottoRank.FIRST, 0),
+                Map.entry(LottoRank.SECOND, 0),
+                Map.entry(LottoRank.THIRD, 0),
+                Map.entry(LottoRank.FOURTH, 0),
+                Map.entry(LottoRank.FIFTH, 0)
+        ));
+
+        final EnumMap<LottoRank, Integer> actual = myLottoTicket.findWinningCountByRank(winningNumbers);
+
+        assertThat(actual).isEqualTo(expectedWinningCountByRank);
     }
 }
